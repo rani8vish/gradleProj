80a47c90a9432e3467fef1cea710656d49cc380e
rvishwakarma <rvishwakarma@RANIV-T460.na.nice.com>
2017-07-11 14:44:51 +0530

    it meets the coding standard.

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,11 @@
 
 public class Hello {
   
-  public static String GREETING = "Hello world!";
+  public static String GREETING = "Hello world!"; 
   
   public static void main(String []args) {
-    
+  System.out.println("Print"+GREETING);
+ 
   }  
 }
 
